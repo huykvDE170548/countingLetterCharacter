b77a9aa7da2a73700663be06ad53754a0d8b9112
Van Huy <kieuvanhuy813@gmail.com>
2023-09-12 09:50:16 +0700

    First Commit

---
+++ src/model/Count.java
@@ -0,0 +1,55 @@
+package model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author THAYCACAC
+ */
+public class Count {
+
+    private Map<Character, Integer> charCounter;
+    private Map<String, Integer> wordCounter;
+             
+    
+    
+    public Count(){
+        charCounter = new HashMap<Character, Integer>();
+        wordCounter = new HashMap<String, Integer>(); 
+    }
+             
+    public void countCharacter(String content) {
+        for (char ch : content.toCharArray()) {             
+            if (Character.isSpaceChar(ch)) {
+                continue;
+            }
+            if (!charCounter.containsKey(ch)) {
+                charCounter.put(ch, 1);
+            } else {
+                charCounter.put(ch, ((int) charCounter.get(ch)) + 1);
+            }
+        }
+        System.out.println(charCounter);
+    }
+    public void countLetter(String content){
+        StringTokenizer tokenizer = new StringTokenizer(content);        
+        while (tokenizer.hasMoreTokens()) {            
+            String token = tokenizer.nextToken();           
+            if (!wordCounter.containsKey(token)) {
+                wordCounter.put(token, 1);
+            } else {
+                wordCounter.put(token, ((int) wordCounter.get(token)) + 1);
+            }
+        }
+        System.out.println(wordCounter);
+    }
+    
+}
